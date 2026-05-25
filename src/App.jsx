f97f755db5e43aa661ca921a504fe16e00348bac
Emryurttas <yurttasemre566@gmail.com>
2026-05-25 23:05:27 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "2rem" }}>
+      <h1>Mon Portfolio</h1>
+      <p>Bienvenue sur mon site 🚀</p>
+    </div>
+  );
+}
+
+export default App;
