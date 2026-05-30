1ee6fe891ff945f7ec8ef64c238cb189cf4e8fd6
Emryurttas <yurttasemre566@gmail.com>
2026-05-30 03:27:16 +0200

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,6 @@ function Header({ darkMode, toggleDarkMode }) {
       <h1 className="logo">Emre Yurttas</h1>
 
       <nav className="nav">
-        <a href="#">Accueil</a>
-        <a href="#projects">Projets</a>
-        <a href="#contact">Contact</a>
-
         <DarkModeToggle darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       </nav>
     </header>
