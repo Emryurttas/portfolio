1c638d6bf416dbb4a187b01dcaf1f6440ba48104
Emryurttas <yurttasemre566@gmail.com>
2026-06-06 00:45:25 +0200

    style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,11 @@ function Footer() {
   return (
     <footer className="footer" id="contact">
       <div className="socials">
-        <a href="https://github.com/" target="_blank">
+        <a href="https://github.com/Emryurttas" target="_blank">
           GitHub
         </a>
 
-        <a href="https://linkedin.com/" target="_blank">
-          LinkedIn
-        </a>
-
-        <a href="mailto:email@gmail.com">
+        <a href="mailto:yurttasemre566@gmail.com">
           Email
         </a>
       </div>
